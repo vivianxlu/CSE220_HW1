ab8f87ad22d9abe174d33a2b2ee95cae5949ecbd
Vivian Lu <85651899+vivianxlu@users.noreply.github.com>
2024-09-07 04:41:56 +0000

    developed initialize_board(...) function

--- src/hw1.c
+++ src/hw1.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <string.h>
 
 #include "hw1.h"
 
@@ -11,7 +12,31 @@ Hint: Consider adding a global variable to store a string large enough to store 
 */
 
 void initialize_board(const char *initial_state, int num_rows, int num_cols) {
+    // [char *initial_state] is a pointer to a character array (a string)
 
+    // Compute the length of the initial_state string
+    int initial_state_length = strlen(initial_state);
+
+    // Edge Cases
+    if (num_rows > MAX_ROWS) {
+        printf("The number of rows that was entered exceeds the limit.");
+        return;
+    } else if (num_cols > MAX_ROWS) {
+        printf("The number of columns that was entered exceeds the limit.");
+        return;
+    } else if (initial_state_length > MAX_ROWS * MAX_COLS) {
+        printf("The length of the board's initial state exceeds the limit.");
+        return;
+    }
+
+    // Print the board
+    int currentPieceIndex = 0;
+    for (int i = 0; i < num_rows; i++) {
+        for (int j = 0; j < num_cols; j++) {
+            printf("%c", initial_state[currentPieceIndex++]);
+        }
+        printf("\n");
+    }
 }
 
 int solve(const char *initial_state, int num_rows, int num_cols, int *num_x, int *num_o) {   
